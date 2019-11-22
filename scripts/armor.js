2d3a880bbb72df73d9c85df9a5b08db049701997
Christopher Ruthenbeck <cruthenbeck@gmail.com>
2019-11-22 18:57:27 -0500

    adjusted label sizes

--- scripts/armor.js
+++ scripts/armor.js
@@ -1,3 +1,13 @@
 import { armorPrice } from "./pricePoints.js";
 
-console.log(armorPrice);
+const price = document.querySelector("[data-price]"),
+  enc = document.querySelector("[data-enc"),
+  hp = document.querySelector("[data-hp");
+
+document.querySelector("form").addEventListener("change", renderSuggestions);
+
+function renderSuggestions(e) {
+  const soak = document.querySelector('[name="soak"]:checked'),
+    defense = document.querySelector('[name="defense"]:checked');
+  // console.log(soak.value, defense.value);
+}
