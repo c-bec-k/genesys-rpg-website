3a650b69ee6efda117f384788f3c24187c79a325
Christopher Ruthenbeck <cruthenbeck@gmail.com>
2020-01-23 14:11:37 -0500

    all ± buttons work

--- creation/vehicle/scripts/prices.js
+++ creation/vehicle/scripts/prices.js
@@ -1,4 +1,13 @@
-const speed = new Map([
+const silhouette =new Map([
+  [1, 0.5],
+  [4, 1],
+  [5, 2],
+  [6, 10],
+  [10, 100]
+]);
+
+
+const maxSpeed = new Map([
   [1, 0],
   [2, 1000],
   [3, 2500],
@@ -59,11 +68,49 @@ const encumbrance = new Map([
 ]);
 
 const consumables = new Map([
-  ['6 hours', 0],
-  ['1–6 days', 10],
-  ['1–3 weeks', 200],
-  ['1–5 months', 400],
-  ['6+ months', 1000]
+  ['hours', 0],
+  ['days', 10],
+  ['weeks', 200],
+  ['months', 400],
+  ['monthsPlus', 1000]
 ]);
 
-export { speed, handling, defense, armor, occupants, encumbrance, consumables };
+
+function getSilMod(sil) {
+  for(let [key, val] of silhouette) {
+    if (sil <= key) { return val }
+  }
+}
+
+function getThreshold(value) {
+  if (value > 50) {
+    return ((value - 50) * 1000) + 4900;
+  }
+  else {
+    return (value - 1) * 100;
+  }
+};
+
+function getCargo(type, value) {
+  for (let  [key, val] of type) {
+    if (value <= key) {return val};
+  }
+};
+
+function determinePrice(vehicle) {
+  const silMod = getSilMod(vehicle.silhouette),
+    speedPrice = maxSpeed.get(vehicle.speed),
+    handlingPrice = handling.get(vehicle.handling),
+    defensePrice = defense.get(vehicle.defense),
+    armorPrice = armor.get(vehicle.armor),
+    consumablesPrice = consumables.get(vehicle.consumables) * ((vehicle.silhouette < 5) ? 1 : 10),
+    httPrice = getThreshold(vehicle.htt),
+    sstPrice = getThreshold(vehicle.sst),
+    occupantsPrice = getCargo(occupants, vehicle.occupants),
+    encPrice = getCargo(encumbrance, vehicle.encumbrance);
+  console.log(consumablesPrice);
+  const totalPrice = (speedPrice + handlingPrice + defensePrice + armorPrice + consumablesPrice + httPrice + sstPrice + occupantsPrice + encPrice) * silMod;
+  return Intl.NumberFormat().format(totalPrice);
+}
+
+export { maxSpeed, handling, defense, armor, occupants, encumbrance, consumables, determinePrice };
--- creation/vehicle/scripts/vehicles.js
+++ creation/vehicle/scripts/vehicles.js
@@ -1,26 +1,82 @@
-import { suggestedNumbers } from './suggestions.js';
-import * as prices from './prices.js';
+import { determinePrice } from './prices.js';
 
-const silhouetteSelection = document.querySelector('[data-silhouette]');
-
-silhouetteSelection.addEventListener('change', updateSuggestions);
-
-function updateSuggestions() {
-  const suggestions = document.querySelectorAll('[data-suggest]');
-  const currentSilhouette = document.querySelector('input[name="silhouette"]:checked').value;
-  console.log(currentSilhouette);
-  suggestions.forEach( suggestion => {
-    const value = suggestion.dataset.suggest;
-    // console.log(suggestedNumbers[currentSilhouette])
-    suggestion.innerText = suggestedNumbers[currentSilhouette][value];
-  });
+let vehicle = {
+  silhouette: 2,
+  speed: 2,
+  handling: 0,
+  defense: 0,
+  armor: 0,
+  htt: 1,
+  sst: 1,
+  occupants: 1,
+  consumables: 'hours',
+  encumbrance: 0,
+  price: 50
 }
 
-const handling = document.querySelector('#handling')
-handling.addEventListener('click', checkAria)
+// for ( let [key, val] of Object.entries(vehicle)) {
+//   console.log(key, val);
+// }
 
-function checkAria(e) {
-  const clicked = e.currentTarget
-  const checked = clicked.getAttribute('aria-checked') === 'true'
-  clicked.setAttribute('aria-checked', !checked)
+document.addEventListener('click', buttonClick);
+document.addEventListener('input', handleInput);
+
+function buttonClick(e) {
+  if (e.target.dataset.direction) {
+    const type = `change_${e.target.dataset.target}`;
+    const payload = { value: parseInt(e.target.dataset.value), target: e.target.dataset.target }
+    const action = { type, payload };
+    reducer(vehicle, action);
+    vehicle.price = determinePrice(vehicle);
+    renderVehicle();
+  };
+}
+
+function reducer(state, action) {
+  const { target, value } = action.payload;
+  switch(action.type) {
+    case 'change_silhouette':
+    case 'change_htt':
+    case 'change_sst':
+    case 'change_occupants':
+    case 'change_encumbrance':
+      modify(value, target);
+      break;
+      // return state.silhouette = state.silhouette + value;
+    case 'decrease':
+      // return modify('minus', action.payload.target);
+    case 'change':
+      // return { ...vehicle, target: value };
+  }  
+}
+
+function renderVehicle() {
+  ["price", "silhouette", "htt", "sst", "occupants", "encumbrance"].forEach( elem => {
+    document.querySelector(`[data-${elem}]`).innerText = vehicle[elem];
+  })
+}
+
+function modify(direction, target) {
+  
+  const el = document.querySelector(`[data-${target}]`);
+  const minNumber = parseInt(el.dataset.min);
+  const maxNumber = parseInt(el.dataset.max);
+  const current = vehicle[target];
+  let adjusted;
+  if ((current === minNumber && direction === -1) || (current === maxNumber && direction === 1)) {
+    adjusted = current;
+  } else {
+    adjusted = (direction === 1) ? current+1 : current-1;
+  }
+  vehicle[target] = adjusted;
+  // el.innerText = state[target];
+  // console.log(vehicle)
+}
+
+
+function handleInput(e) {
+  const { name, value } = e.target;
+  vehicle[name] = (name === 'consumables') ? value : parseInt(value);
+  vehicle.price = determinePrice(vehicle);
+  renderVehicle();
 }
